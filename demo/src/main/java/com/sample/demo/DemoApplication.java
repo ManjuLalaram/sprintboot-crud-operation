7848d9309d6531cabf3108c9712eaf8b6b7c6c11
Hp <Hp@DESKTOP-CE1L4QN>
2023-12-24 19:06:03 +0530

    spring boot sample demo

---
+++ demo/src/main/java/com/sample/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.sample.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+	}
+
+}
